95ed3c1fd4de1b9d90b68f4d0f6de5a9463dd59e
Tani <ysolakey1116@gmail.com>
2022-08-14 10:41:08 +0900

    Setup: Babel & express

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', async (_req, res) => {
+  return res.status(200).send({
+    message: 'Hello World!!',
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`dev server running at: http://localhost:${PORT}/`)
+})
