4b23e0b2b0e6d8e1b452790849641ff74ddf39be
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-16 17:03:01 +0900

    Create: socket.io(fake)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import express from 'express'
+import express from "express";
+import socket from "socket.io";
+import http from "http";
+import cors from "cors";
 
-const app = express()
-const PORT = 4000
+const app = express();
+const server = http.Server(app);
+const io = socket(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const PORT = 4000;
 
-app.get('/', async (_req, res) => {
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (_req, res) => {
   return res.status(200).send({
-    message: 'Hello World!!',
-  })
-})
+    message: "Hello World!!",
+  });
+});
 
-app.listen(PORT, () => {
-  console.log(`dev server running at: http://localhost:${PORT}/`)
-})
+io.on("connection", (socket) => {
+  console.log("connected", socket.id);
+});
+
+server.listen(PORT, () => {
+  console.log(`dev server running at: http://localhost:${PORT}/`);
+});
