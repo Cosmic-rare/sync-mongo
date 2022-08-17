e1b0409fdda5a097bf56aa0cec0b2a9caa7895fe
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-17 15:32:35 +0900

    Create: use validation, connect mongodb

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import express from "express";
 import socket from "socket.io";
 import http from "http";
 import cors from "cors";
+import mongoose from "mongoose";
+import { check, validationResult } from "express-validator";
+
+const Task1Type = new mongoose.Schema({
+  _uid: String,
+  _rev: Number,
+  _deleted: Boolean,
+  title: String,
+  done: Boolean,
+  _createdAt: Number,
+  _updatedAt: Number,
+  _hash: String,
+});
 
 const app = express();
 const server = http.Server(app);
@@ -11,16 +24,43 @@ const io = socket(server, {
   },
 });
 const PORT = 4000;
+const command_type = ["create", "update", "delete"];
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/", (req, res) => {
-  req.body.commands.map((val) => {
-    console.log(val.type);
-  });
-  return res.status(200).json({});
-});
+mongoose.connect("mongodb://localhost/sync-db");
+const Task1 = mongoose.model("Task1", Task1Type);
+
+app.post(
+  "/",
+  [
+    check("type").not().isEmpty(),
+    check("data._id").isUUID(),
+    check("data._rev").isInt(),
+    check("data._deleted").isBoolean(),
+    check("data.title").not().isEmpty(),
+    check("data.done").isBoolean(),
+    check("data._createdAt").isInt(),
+    check("data._updatedAt").isInt(),
+    check("data._hash").isMD5(),
+    check("data.id").not().isEmpty(),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    if (command_type.includes(req.body.type)) {
+      console.log(req.body.data.id);
+    }
+
+    return res.status(200).json({});
+  }
+);
 
 io.on("connection", (socket) => {
   console.log("connected", socket.id);
