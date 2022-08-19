4cdff0f844cdbb3818bc83f4183657e048fdd5cf
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-20 08:29:35 +0900

    Create: one item sync

--- src/route.js
+++ src/route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { validationResult } from "express-validator";
 import syncValidator from "./syncValidator";
-import { Task1 } from "./schema";
+import { Task1, Task2 } from "./schema";
 
 const router = express.Router();
 const command_type = ["create", "update", "delete"];
@@ -31,10 +31,45 @@ router.post("/", syncValidator, async (req, res) => {
     if (task._rev < data._rev) {
       // copy "task" from Task1 to Task2
       await Task1.findOneAndUpdate({ _id: data._id }, data);
+
+      return new Task2({
+        _uid: task._id,
+        _rev: task._rev,
+        _deleted: task._deleted,
+        title: task.title,
+        done: task.done,
+        _createdAt: task._createdAt,
+        _updatedAt: task._updatedAt,
+        _hash: task._hash,
+        _movedAt: Date.now(),
+      })
+        .save()
+        .then(() => {
+          console.log("move to table 2 and update table 1");
+          return res.status(200).json({});
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({});
+        });
     } else {
-      // Insert Table2 only
+      data._uid = data._id;
+      delete data._id;
+
+      return new Task2({
+        ...data,
+        _movedAt: Date.now(),
+      })
+        .save()
+        .then(() => {
+          console.log("move to table 2 only");
+          return res.status(200).json({});
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({});
+        });
     }
-    return res.status(200).json({});
   } else {
     return new Task1(data)
       .save()
