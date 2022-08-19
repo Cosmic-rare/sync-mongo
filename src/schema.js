3c8501d582e704733d9a6c9f23af2ac10e7b87d5
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-19 09:13:38 +0900

    Create: Task2 Table

--- src/schema.js
+++ src/schema.js
@@ -11,4 +11,17 @@ const Task1Type = new mongoose.Schema({
   _hash: String,
 });
 
+const Task2Type = new mongoose.Schema({
+  _uid: String,
+  _rev: Number,
+  _deleted: Boolean,
+  title: String,
+  done: Boolean,
+  _createdAt: Number,
+  _updatedAt: Number,
+  _hash: String,
+  _movedAt: Number,
+});
+
 export const Task1 = mongoose.model("Task1", Task1Type);
+export const Task2 = mongoose.model("Task2", Task2Type);
