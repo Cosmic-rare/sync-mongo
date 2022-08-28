327f12878bb33e58d80b2c40b80c5eeef20d2635
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-29 05:45:35 +0900

    Create: simple validation logic

--- src/route.js
+++ src/route.js
@@ -1,30 +1,60 @@
 import express from "express";
 import Ajv from "ajv";
+import { Task } from "./schema";
 
 const router = express.Router();
 const ajv = new Ajv();
 
-const schema = {
-  required: ["type"],
+const bodySchema = {
+  required: ["type", "datas"],
   type: "object",
   properties: {
     type: {
       type: "string",
-      pattern: "create|update|delete",
+      pattern: "^(create|update|delete)+$",
+    },
+    datas: {
+      type: "array",
     },
   },
 };
 
-const validate = ajv.compile(schema);
+const taskSchema = {
+  required: ["title", "_id"],
+  type: "object",
+  properties: {
+    title: {
+      type: "string",
+    },
+    _id: {
+      type: "string",
+    },
+  },
+};
+
+const bodyValidate = ajv.compile(bodySchema);
+const taskValidate = ajv.compile(taskSchema);
 
 router.post("/", async (req, res) => {
-  const valid = validate(req.body);
+  const errorTasks = [];
+  const sucsessTasks = [];
 
-  if (!valid) {
-    return res.status(400).json({ errors: validate.errors });
+  const bodyValid = bodyValidate(req.body);
+
+  if (!bodyValid) {
+    return res.status(400).json({ errors: bodyValidate.errors });
   }
 
-  return res.status(200).json({ status: "sucsess" });
+  req.body.datas?.map((val, index) => {
+    const taskValid = taskValidate(val);
+    if (!taskValid) {
+      errorTasks.push(val?._id);
+    } else {
+      sucsessTasks.push(val._id);
+    }
+  });
+
+  return res.status(200).json({ sucsess: sucsessTasks, error: errorTasks });
 
   /*
   if req.body.type == delete
--- src/schema.js
+++ src/schema.js
@@ -1,27 +1,15 @@
 import mongoose from "mongoose";
 
-const Task1Type = new mongoose.Schema({
-  _id: String,
-  _rev: Number,
-  _deleted: Boolean,
-  title: String,
-  done: Boolean,
-  _createdAt: Number,
-  _updatedAt: Number,
-  _hash: String,
-});
-
-const Task2Type = new mongoose.Schema({
+const TaskType = new mongoose.Schema({
   _uid: String,
-  _rev: Number,
-  _deleted: Boolean,
   title: String,
   done: Boolean,
   _createdAt: Number,
   _updatedAt: Number,
   _hash: String,
-  _movedAt: Number,
+  _rev: Number,
+  _deleted: Boolean,
+  _v: Number,
 });
 
-export const Task1 = mongoose.model("Task1", Task1Type);
-export const Task2 = mongoose.model("Task2", Task2Type);
+export const Task = mongoose.model("Task", TaskType);
